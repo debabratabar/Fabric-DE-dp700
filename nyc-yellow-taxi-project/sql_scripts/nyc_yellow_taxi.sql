048f7e4dde9f7dd5a01acf5f5cbdfd03975ff3d1
Debabrata <48706465+debabratabar@users.noreply.github.com>
2025-12-06 04:43:31 -0800

    Add stored procedure to insert NYC yellow taxi data

--- nyc-yellow-taxi-project/sql_scripts/nyc_yellow_taxi.sql
+++ nyc-yellow-taxi-project/sql_scripts/nyc_yellow_taxi.sql
@@ -84,24 +84,58 @@ AS
             dbo.nyctaxi_yellow ;
 GO
 
+CREATE PROCEDURE
+    dbo.insert_nyctaxi_yellow
+AS
+    INSERT INTO
+        dbo.nyctaxi_yellow (
+	vendor ,tpep_pickup_datetime,tpep_dropoff_datetime 	,pu_borough 	,pu_zone ,do_borough 	,do_zone 	
+    ,payment_method ,passenger_count ,trip_distance ,total_amount )
+        SELECT
+            case when [VendorID]= 1 then 'Creative Mobile Technologies'
+             when [VendorID]= 2 then 'Curb Mobility'
+             when [VendorID]= 6 then 'Myle Technologies Inc'
+             when [VendorID]= 7 then 'Helix'
+            else 'Unknown' end    as vendor ,
+            CAST ( [tpep_pickup_datetime] as Date ) AS tpep_pickup_datetime,
+			cast ( [tpep_dropoff_datetime] as Date ) As tpep_dropoff_datetime ,
+            nycl.[Borough] as pu_borough , 
+            nycl.[Zone] as pu_zone , 
+            nycl2.[Borough] as bo_borough ,
+            nycl2.[Zone] as bo_zone ,
+			case when [payment_type] = 0 then 'Flex Fare trip' 
+                when [payment_type] = 1 then 'Credit card' 
+                when [payment_type] = 2 then 'Cash' 
+                when [payment_type] = 3 then 'No charge' 
+                when [payment_type] = 4 then 'Dispute' 
+                when [payment_type] = 5 then 'Unknown'
+                when [payment_type] = 6 then 'Voided trip'
+            else 'Unknown' end as payment_method , 
+            [passenger_count] , 
+            [trip_distance],
+			[total_amount]
+FROM [stg].[nyctaxi_yellow ] nycy 
+       left join  [stg].[nyctaxi_zone_lkp] nycl on nycy.[PULocationID]= nycl.[LocationID]
+        left join    [stg].[nyctaxi_zone_lkp] nycl2 on nycy.[DOLocationID]= nycl2.[LocationID];
+GO
 
 -- ##############################################
 
-select * from dbo.nyctaxi_yellow
+-- select * from dbo.nyctaxi_yellow
 
-    SELECT * FROM metadata.processing_log
+--     SELECT * FROM metadata.processing_log
 
-        SELECT * FROM stg.nyctaxi_yellow
+--         SELECT * FROM stg.nyctaxi_yellow
 
-        SELECT distinct VendorID FROM stg.nyctaxi_yellow
-SELECT * from stg.nyctaxi_zone_lkp
+--         SELECT distinct VendorID FROM stg.nyctaxi_yellow
+-- SELECT * from stg.nyctaxi_zone_lkp
 
-SELECT top 1 latest_processed_pickup
-from metadata.processing_log
-where processed_tbl_name = 'stg.nyctaxi_yellow'
-ORDER by latest_processed_pickup desc
+-- SELECT top 1 latest_processed_pickup
+-- from metadata.processing_log
+-- where processed_tbl_name = 'stg.nyctaxi_yellow'
+-- ORDER by latest_processed_pickup desc
 
 
-SELECT MAX(tpep_pickup_datetime) , MIN(tpep_pickup_datetime) from stg.nyctaxi_yellow 
+-- SELECT MAX(tpep_pickup_datetime) , MIN(tpep_pickup_datetime) from stg.nyctaxi_yellow 
 
-SELECT MAX(tpep_pickup_datetime) , MIN(tpep_pickup_datetime) from dbo.nyctaxi_yellow 
+-- SELECT MAX(tpep_pickup_datetime) , MIN(tpep_pickup_datetime) from dbo.nyctaxi_yellow 
